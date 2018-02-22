4ca673e0663e0c3a4475e40f36790ba3a4c5b885
Paulo <prgomesr@gmail.com>
2018-02-22 17:56:24 -0300

    update 55:34

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
+
+import { TableModule } from 'primeng/table';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ItemCadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/item-cadastro/item-cadastro.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../item.service';
+
+@Component({
+  selector: 'app-item-cadastro',
+  templateUrl: './item-cadastro.component.html',
+  styleUrls: ['./item-cadastro.component.css']
+})
+export class ItemCadastroComponent implements OnInit {
+  itens = [];
+  constructor(private itemService: ItemService) { }
+
+  ngOnInit() {
+  }
+
+  consultar() {
+    this.itemService.listar().subscribe(dados => this.itens = dados);
+  }
+
+}
